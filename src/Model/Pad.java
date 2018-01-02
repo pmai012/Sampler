be91659b84e6c4eb62bbc741ed9318acf357bdb5
kolone13 <30953267+kolone13@users.noreply.github.com>
2018-01-02 13:58:17 +0100

    Kleine Funktionen zu Pad hinzugefügt

--- src/Model/Pad.java
+++ src/Model/Pad.java
@@ -1,7 +1,43 @@
 package Model;
 
+import com.mpatric.mp3agic.InvalidDataException;
+import com.mpatric.mp3agic.Mp3File;
+import com.mpatric.mp3agic.UnsupportedTagException;
+
+import java.io.IOException;
+
 /**
  * Created by User on 21.12.2017.
  */
 public class Pad {
+private Mp3File file ;
+private long startpoint;
+private long endpoint;
+    public void play() {
+        
+    }
+
+    public void delete() {
+    }
+
+
+    public void loadSound(String path) {
+        try {
+            file = new Mp3File(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (UnsupportedTagException e) {
+            e.printStackTrace();
+        } catch (InvalidDataException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setStartpoint(long time) {
+       startpoint = time;
+    }
+
+    public void setEndpoint(long time) {
+        endpoint = time;
+    }
 }
