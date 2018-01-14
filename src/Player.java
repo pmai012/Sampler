adad5752c5b7c1a2b46c86088df61db1096bf677
kolone13 <30953267+kolone13@users.noreply.github.com>
2018-01-14 13:12:26 +0100

    I create Player class

--- src/Player.java
+++ src/Player.java
@@ -2,18 +2,22 @@ import ddf.minim.AudioOutput;
 import ddf.minim.AudioPlayer;
 import ddf.minim.spi.AudioOut;
 import ddf.minim.spi.AudioRecordingStream;
+import de.hsrm.mi.eibo.simpleplayer.SimpleAudioPlayer;
 import de.hsrm.mi.eibo.simpleplayer.SimpleMinim;
+import javafx.application.Platform;
 
 /**
  * Created by Julian on 11.01.2018.
  */
-public class Player extends SimpleMinim {
+public class Player  {
     private AudioOutput audioOutput;
-    private SimpleMinim minim;
+    SimpleAudioPlayer player;
+    SimpleMinim minim;
 
     public void setFile(String path){
-        minim.loadMP3File(path);
-        
+        minim = new SimpleMinim(true);
+        player = minim.loadMP3File(path);
+
     }
 
     public AudioOutput getAudioOutput() {
