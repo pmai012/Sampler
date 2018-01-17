abcab3aeaa535875321526cacbb85a43fc82bca9
DA31 <deniz.aydar@gmx.de>
2018-01-17 18:40:09 +0100

    radiobutton hinzugefügt

--- src/View/SoundView.java
+++ src/View/SoundView.java
@@ -2,20 +2,42 @@ package View;
 
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
 /**
  * Created by User on 21.12.2017.
  */
 public class SoundView extends Pane {
 
+    private VBox soundpane;
     private ListView soundlist;
-    private Label test;
+    private Label audioFX;
+    private RadioButton fxOn, fxOff;
 
     public SoundView(){
 
+
         soundlist = new ListView();
-        test = new Label("hallo");
-        this.getChildren().addAll(soundlist);
+        fxOn = new RadioButton("An");
+        fxOff = new RadioButton("Aus");
+        fxOff.setSelected(true);
+
+        ToggleGroup activeGroup =new ToggleGroup();
+        activeGroup.getToggles().addAll(fxOn,fxOff);
+        audioFX = new Label("Audio-FX");
+
+        soundpane = new VBox(10);
+        this.getChildren().add(soundpane);
+
+        soundpane.getChildren().addAll(soundlist);
+        soundpane.getChildren().add(audioFX);
+        soundpane.getChildren().addAll(fxOn,fxOff);
+
+        audioFX.getStyleClass().add("title");
+        fxOff.getStyleClass().add("title");
+        fxOn.getStyleClass().add("title");
     }
 }
