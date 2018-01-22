1c240063ff1e3eecff1691e22da6b6ce31a8b60e
kolone13 <30953267+kolone13@users.noreply.github.com>
2018-01-22 10:25:21 +0100

    Opendialog gibt es nun auch

--- src/Controller/SaveOpenDialog.java
+++ src/Controller/SaveOpenDialog.java
@@ -11,6 +11,13 @@ import java.io.File;
  */
 public class SaveOpenDialog {
 
+
+    /**
+     * Gibt ein Dialog zum Speichern von Dateien aus.
+     * @param stage Die Stage, wo der Dialog erscheinen soll
+     * @param title Den Titel des DIaloges
+     * @return Gibt den ausgewählten Speicherort zurück
+     */
     public String Savedialog(Stage stage, String title){ //
 
         FileChooser fileChooser = new FileChooser();
@@ -22,10 +29,27 @@ public class SaveOpenDialog {
     }
 
 
+    /**
+     * Gibt ein Dialog für die Auswahl eines Ordners an
+     * @param stage Die Stage, wo der Dialog erscheinen soll
+     * @param title Den Titel des Dialoges
+     * @return Gibt den Absoluten Pfad zurueck
+     */
     public String DirectionDialog(Stage stage, String title){
          DirectoryChooser directoryChooser = new DirectoryChooser();
         File file =
                 directoryChooser.showDialog(stage);
        return file.getAbsolutePath();
+    }
+
+
+    /**
+     * Öffnet einen Opendialog
+     * @param stage Nur die Stage wo der Dialog ausgegeben werden soll
+     * @return Die ausgewählte File
+     */
+    public File OpenDialog(Stage stage){
+        FileChooser fileChooser = new FileChooser();
+        return fileChooser.showOpenDialog(stage);
     }
 }
