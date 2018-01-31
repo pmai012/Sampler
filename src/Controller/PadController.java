fea8ba79746c00dd0c7893698263a6a9efcad220
DA31 <deniz.aydar@gmx.de>
2018-01-31 23:02:43 +0100

    baustelle

--- src/Controller/PadController.java
+++ src/Controller/PadController.java
@@ -11,7 +11,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-//import javafx.scene.control.TextInputDialog;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.control.ButtonType;
 import javafx.scene.input.*;
 
